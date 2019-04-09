cca43846f2f848e42e68815a95443dfd22c87972
automaticdai <automatic.dai@gmail.com>
2019-04-09 21:22:11 +0100

    changed ga_main

--- ga_main.m
+++ ga_main.m
@@ -1,4 +1,27 @@
-% main code for minimising via GA
+%% initilize parameters
+% unit: 10us
+simu.time = 1.0;    % time of simulation
+
+afbs_params = [0];
+
+% [C, Th, Tl, alpha, D]
+taskset = [ 50, 200, 1000, 0.5,  0 ...
+                 50, 200, 1000, 0.5,  0 ...
+                 50, 200, 1000, 0.5,  0 ...
+                 50, 200,     -1,  -1, 200];
+
+% Ts reference
+tsref1 = 2.0;
+tsref2 = 2.0;
+tsref3 = 2.0;
+
+% Ts minimal requirement
+tsmin1 = 5.0;
+tsmin2 = 5.0;
+tsmin3 = 5.0;
+
+
+%% main code for minimising via GA
 
 % Ideal Point
 
@@ -13,7 +36,7 @@ LB = [0 0 0];
 UB = [1000 1000 1000];
 
 % Optimising options
-opts = optimoptions('ga','PlotFcn',@gaplotbestf, 'PopulationSize',100,'MaxGenerations',300,'CrossoverFcn',@crossoversinglepoint ,'CrossoverFraction',0.8,'MutationFcn',{@mutationuniform,0.2},'Display','iter');
+opts = optimoptions('ga','PlotFcn',@gaplotbestf, 'PopulationSize',5,'MaxGenerations',5,'CrossoverFcn',@crossoversinglepoint ,'CrossoverFraction',0.8,'MutationFcn',{@mutationuniform,0.2},'Display','iter');
 
 % GA main function
 [x,fval,exitflag] = ga(objFcn, nVars,[],[],[],[],LB,UB,[], [1 2],opts);
--- myFitness.m
+++ myFitness.m
@@ -1,8 +1,13 @@
-
 % Fitness function
 % min f(x) = 100 (x1^2 - x2)^2 + (1-x1)^2
-
+% x = [T_H, T_L, alpha]
 function y = myFitness(x)
-y = 100 * (x(1)^2  - x(2))^2 + (1-x(1))^2 + x(3);
-end
 
+
+
+
+y = 100 * (x(1)^2  - x(2))^2 + (1-x(1))^2 + x(3);
+
+
+
+end
