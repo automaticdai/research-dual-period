9f81a156ebb215901c95915e30ccbaaa1ebe2c50
automaticdai <automatic.dai@gmail.com>
2019-04-10 23:28:44 +0100

    added random searching method

---
+++ gen_random_param.m
@@ -0,0 +1,22 @@
+function [x] = gen_random_param()
+%Generate a random parameters for dual period tasks
+%   x = [T_H T_L alpha]
+% Following constraints are used:
+% LB = [200   200   0    200 200   0   200  200   0];
+% UB = [500   500 100    500 500 100   500  500 100];
+TL1 = randi([200 500], 1, 1);
+TH1 = randi([200 TL1], 1, 1);
+alpha1 = randi([0 100], 1, 1);
+
+TL2 = randi([200 500], 1, 1);
+TH2 = randi([200 TL2], 1, 1);
+alpha2 = randi([0 100], 1, 1);
+
+TL3 = randi([200 500], 1, 1);
+TH3 = randi([200 TL3], 1, 1);
+alpha3 = randi([0 100], 1, 1);
+
+x = [TH1 TL1 alpha1 TH2 TL2 alpha2 TH3 TL3 alpha3];
+
+end
+
---
+++ main_random.m
@@ -0,0 +1,38 @@
+% random search method for benchmark
+
+clc; clear;
+
+rng(1, 'twister')
+
+num_of_iteration = 20;
+num_of_population = 50;
+
+result_fitness_best = [];
+result_fitness_mean = [];
+
+fitness_best = 0;
+
+for iter = 1:num_of_iteration
+    
+    fprintf("iteration: %d", iter);
+    
+    fitness_sum = 0;
+    
+    for i = 1:num_of_population
+        % call fitness function
+        x = gen_random_param();
+        fitness = 1.0 - myFitness(x);
+        fitness_sum = fitness_sum + fitness;
+        if (fitness > fitness_best) 
+            fitness_best = fitness;
+            x_best = x;
+        end
+    end
+
+    result_fitness_best = [result_fitness_best fitness_best];
+	result_fitness_mean = [result_fitness_mean fitness_sum / num_of_population];
+end
+
+result_fitness_best
+result_fitness_mean
+x_best
